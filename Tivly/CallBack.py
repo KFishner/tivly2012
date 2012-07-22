b8a5dd69ce507103b58593e9f185dd52b2840c3f
Bryan Antigua <antigua_B@hotmail.com>
2012-07-22 14:07:29 -0700

    new files

--- Tivly/CallBack.py
+++ Tivly/CallBack.py
@@ -3,8 +3,8 @@ Created on Jul 6, 2012
 
 @author: bryanantigua
 '''
-from CardSpring import *
-from Tivly.models import *
+from Tivly.models import UserPoints,MyRewards,Rewards
+from datetime import datetime
 
 import urllib2
 import urllib
@@ -15,18 +15,9 @@ def callBack(request):
     if request.method == "POST":
         csid = request.POST['user_id']
         userPoints = UserPoints.objects.filter(csID =csid, businessID = request.POST['business_id'])[0]
-             
-        if request.POST['event_type'] == 'settlement' or request.POST['event_type'] == 'authorization':
-#            userPoints.points = userPoints.points + 1
-#            userPoints.visits = userPoints.visits + 1
-#            userPoints.save()
-            continue
         
-        if request.POST['event_type'] == 'redemption':
-            rewardLookUp = Rewards.objects.filter(appID = request.POST['app_id'])[0]
-            myReward = MyRewards.objects.filter(csID = csid, reward = rewardLookUp)
-            myReward.used = True
-            myReward.save()
-            
-#            userPoints.points = userPoints.points - reward.pointsNeeded
-#            userPoints.save()
+        rewardLookUp = Rewards.objects.filter(appID = request.POST['app_id'])[0]
+        myReward = MyRewards.objects.filter(csID = csid, reward = rewardLookUp)
+        myReward.used = True
+        myReward.dateUsed = datetime.now()
+        myReward.save()
