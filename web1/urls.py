6d08e0db1daa1f07ec30c815b8d0c37c0c8a9a33
Bryan Antigua <antigua_B@hotmail.com>
2012-07-21 21:34:43 -0700

    au url

--- web1/urls.py
+++ web1/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     
     url(r'^me/$', accountInfo),
     url(r'^logout/$', logout),
-    url(r'^aboutUs/$', aboutUs),
+    url(r'^aboutUs/$', aboutus),
     url(r'jobs/$', jobs),
     url(r'privacy/$', privacy),
     url(r'contact/$', contact),
