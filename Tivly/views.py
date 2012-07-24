2e521c416f0ab17f4f1ac41fe3c5a8400e09d9fb
Bryan Antigua <antigua_B@hotmail.com>
2012-07-24 14:33:29 -0700

    tivly change

--- Tivly/views.py
+++ Tivly/views.py
@@ -16,7 +16,7 @@ from django.views.decorators.csrf import csrf_exempt
 def login (request):
     #template variables...       
     FACEBOOK_APP_ID = settings.FACEBOOK_APP_ID
-    facebookRedirect = settings.FACEBOOK_REDIRECT_URI
+    facebookRedirect = 'https://'+settings.FACEBOOK_REDIRECT_URI
     redirectURL = 'https://'+settings.URL + '/splash'
 
     csid = request.COOKIES.get('csID',None)
