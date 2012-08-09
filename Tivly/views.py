577dfd17c8cb92eca50b6d4a3fecd05fd8278d40
Bryan Antigua <antigua_B@hotmail.com>
2012-08-09 13:01:38 -0400

    finding cc prob

--- Tivly/views.py
+++ Tivly/views.py
@@ -195,10 +195,10 @@ def accountInfo(request):
     redirect = 'https://www.tivly.com/home'
     CARDSPRING_APP_ID = settings.CARDSPRING_APP_ID
     
-#    securityToken = IDGenerator(32)
-#    timestamp = datetime.now()
-#    key = settings.CARDSPRING_APP_ID
-#    raw = '{'+securityToken+'}:{'+str(datetime.now())+'}:{XE6AN1}'
+    securityToken = IDGenerator(32)
+    timestamp = datetime.now()
+    key = settings.CARDSPRING_APP_ID
+    raw = '{'+securityToken+'}:{'+str(datetime.now())+'}:{XE6AN1}'
 #    hashed = hmac.new(key, raw, sha1)
 #    digestedHash = hashed.digest()
     return render_to_response('myaccount.html', locals(), context_instance= RequestContext(request))
