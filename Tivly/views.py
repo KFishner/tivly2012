6b10bc8fc4fbd20781c42b1d9acb0fe301e3a0a6
Bryan Antigua <antigua_B@hotmail.com>
2012-08-12 22:15:24 -0400

    cc fix

--- Tivly/views.py
+++ Tivly/views.py
@@ -199,7 +199,8 @@ def accountInfo(request):
     securityToken = IDGenerator(32)
     timestamp = time.time()
     key = settings.CARDSPRING_APP_SECRET
-    raw = '{'+securityToken+'}:{'+str(timestamp)+'}:{'+csid+'}'
+#    raw = '{'+securityToken+'}:{'+str(timestamp)+'}:{'+csid+'}'
+    raw = securityToken+':'+str(timestamp)+':'+csid
     hashed = hmac.new(key, raw, sha1)
     digestedHash = hashed.hexdigest()
     return render_to_response('myaccount.html', locals(), context_instance= RequestContext(request))
