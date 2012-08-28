8dd729b03e08847015323d74a254d45f7a064104
Peter <pgrabows@princeton.edu>
2012-08-28 17:04:40 -0400

    context proc

--- web1/settings.py
+++ web1/settings.py
@@ -91,7 +91,7 @@ STATIC_URL = '/static/'
 TEMPLATE_CONTEXT_PROCESSORS = 
 ('Tivly.context_processors.cardspring',
 #"django.contrib.auth.context_processors.auth",
-#"django.core.context_processors.debug",
+"django.core.context_processors.debug",
 #"django.core.context_processors.i18n",
 #"django.core.context_processors.media",
 #"django.core.context_processors.static",
