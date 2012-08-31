f8548db1319e3ff1c3f89f053d3504881c88840a
Bryan Antigua <antigua_B@hotmail.com>
2012-08-31 16:26:54 -0400

    rb

--- Tivly/views.py
+++ Tivly/views.py
@@ -330,7 +330,7 @@ def howto(request,number):
             introReward = reward.reward
             break
         
-    business = Businesses.objects.filter(businessID = introReward.reward.businessID)
+    business = Businesses.objects.filter(businessID = introReward.businessID)
     
     #these are for authentication with CardSpring JS library...
     securityToken = IDGenerator(32)
