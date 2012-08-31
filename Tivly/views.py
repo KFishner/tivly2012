48c14f55de7105bf74d60904fb6e4787e365e1f9
Bryan Antigua <antigua_B@hotmail.com>
2012-08-31 16:31:20 -0400

    was calling things on a query set like an idiot

--- Tivly/views.py
+++ Tivly/views.py
@@ -330,7 +330,7 @@ def howto(request,number):
             introReward = reward.reward
             break
         
-    business = Businesses.objects.filter(businessID = introReward.businessID)
+    business = Businesses.objects.filter(businessID = introReward.businessID)[0]
     
     #these are for authentication with CardSpring JS library...
     securityToken = IDGenerator(32)
