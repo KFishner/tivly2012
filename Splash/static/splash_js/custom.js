b4192375775850d6d6a35d245bf65cfd7a9fa8dd
KFishner <KFishner@gmail.com>
2012-09-25 19:26:20 -0700

    M

--- Splash/static/splash_js/custom.js
+++ Splash/static/splash_js/custom.js
@@ -69,7 +69,7 @@ $(document).ready(function(){
 	jQuery("#feature-slider").slides({
 		next:'next',
 		prev:'prev',
-		generatePagination:false,
+		generatePagination:true,
 		autoHeight:true,
 		bigTarget:false,
 		hoverPause:true,
